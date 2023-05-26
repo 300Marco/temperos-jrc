0b4aaea7d7201ebffd68d213d1edc0a92d9ee856
Marco Santana <marcos.maio96@gmail.com>
2023-05-26 14:15:30 -0300

    update: footer wave

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,14 @@
-import { FooterContainer } from './styles'
+import { FooterContainer, FooterWave } from './styles'
+
+import ondaFooter from '../../assets/onda-footer.svg'
 
 export function Footer() {
   return (
     <FooterContainer>
+      <FooterWave>
+        <img src={ondaFooter} alt="" />
+      </FooterWave>
+
       <h3>Footer</h3>
     </FooterContainer>
   )
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,8 +1,27 @@
 import { styled } from '../../styles'
 
 export const FooterContainer = styled('footer', {
+  position: 'relative',
+
   width: '100%',
   padding: '2rem',
-  marginTop: '8.25rem',
+  // marginTop: '8.25rem',
+  marginTop: '13.875rem',
   backgroundColor: '$gray800',
+})
+
+export const FooterWave = styled('div', {
+  position: 'absolute',
+  bottom: '95%',
+  left: 0,
+
+  width: '100%',
+  height: '5.625rem',
+
+  display: 'flex',
+  alignItems: 'end',
+
+  img: {
+    width: '100%',
+  },
 })
